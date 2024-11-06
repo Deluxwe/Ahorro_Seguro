faa2724f91faab01528c472814a4364d54c98737
Deluxwe <141252323+Deluxwe@users.noreply.github.com>
2024-11-06 08:25:54 -0500

    Update NodoArbol.java

--- Ahorro_seguro/src/main/java/com/mycompany/ahorro_seguro/NodoArbol.java
+++ Ahorro_seguro/src/main/java/com/mycompany/ahorro_seguro/NodoArbol.java
@@ -3,15 +3,39 @@ package com.mycompany.ahorro_seguro;
 
 
 public class NodoArbol {
-
-    public CreditoVigente credito;
-    public NodoArbol izquierdo;
-    public NodoArbol derecho;
+    private CreditoVigente credito;
+    private NodoArbol izquierdo;
+    private NodoArbol derecho;
 
     public NodoArbol(CreditoVigente credito) {
         this.credito = credito;
         this.izquierdo = null;
         this.derecho = null;
     }
+
+    public CreditoVigente getCredito() {
+        return credito;
+    }
+
+    public void setCredito(CreditoVigente credito) {
+        this.credito = credito;
+    }
+
+    public NodoArbol getIzquierdo() {
+        return izquierdo;
+    }
+
+    public void setIzquierdo(NodoArbol izquierdo) {
+        this.izquierdo = izquierdo;
+    }
+
+    public NodoArbol getDerecho() {
+        return derecho;
+    }
+
+    public void setDerecho(NodoArbol derecho) {
+        this.derecho = derecho;
+    }
+    
 }
 
